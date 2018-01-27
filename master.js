cbd6740d41b4ae9f2e4188f39cea2f05df8ad881
Peter Allan <alwynallan@gmail.com>
2018-01-26 21:02:05 -0500

    https://github.com/standard/standard

--- master.js
+++ master.js
@@ -1,233 +1,241 @@
 // Copyright (C) 2017 alwynallan@gmail.com
 // Merry Christmas Sue!
 
-var http = require('http');
-var fs = require('fs');
-const cp = require('child_process');
+var http = require('http')
+var fs = require('fs')
+const cp = require('child_process')
 
 // https://www.npmjs.com/package/node-cron because it has destroy()
-var cron = require('node-cron');
+var cron = require('node-cron')
 
 // https://github.com/simonlast/node-persist
-var storage = require('node-persist');
+var storage = require('node-persist')
 
 // https://github.com/plasticrake/tplink-smarthome-api
-const { Client } = require('tplink-smarthome-api');
+const { Client } = require('tplink-smarthome-api')
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-async function bulb_page(res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<!DOCTYPE html>\n');
-  res.write('<html><head><title>Color Bulbs</title>\n');
-  res.write('<link rel="stylesheet" href="demo.css">\n');
-  res.write('<link rel="stylesheet" href="form-basic.css">\n');
-  res.write('<script>\nfunction ajax_get(payload){\n'); // no response considered
-  res.write('  var xhttp = new XMLHttpRequest();\n');
-  res.write('  xhttp.open("GET", payload, true);\n');
-  res.write('  xhttp.send();\n');
-  res.write('  document.activeElement.blur();\n'); // remove focus from button
-  res.write('}\n</script>\n');
-  res.write('</head><body>\n');
-  res.write('<header><h1>Color lightbulbs on local network</h1></header>\n');
-  res.write('<div class="main-content"><div class="form-basic">\n');
-  var client = new Client();
-  let sleeper = sleep(400);
-  var bulb_count = 0;
-  client.startDiscovery();
+async function bulbPage (res) {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.write('<!DOCTYPE html>\n')
+  res.write('<html><head><title>Color Bulbs</title>\n')
+  res.write('<link rel="stylesheet" href="demo.css">\n')
+  res.write('<link rel="stylesheet" href="form-basic.css">\n')
+  res.write('<script>\nfunction ajax_get(payload){\n') // no response considered
+  res.write('  var xhttp = new XMLHttpRequest();\n')
+  res.write('  xhttp.open("GET", payload, true);\n')
+  res.write('  xhttp.send();\n')
+  res.write('  document.activeElement.blur();\n') // remove focus from button
+  res.write('}\n</script>\n')
+  res.write('</head><body>\n')
+  res.write('<header><h1>Color lightbulbs on local network</h1></header>\n')
+  res.write('<div class="main-content"><div class="form-basic">\n')
+  var client = new Client()
+  let sleeper = sleep(400)
+  var bulbCount = 0
+  client.startDiscovery()
   client.on('bulb-new', (bulb) => {
-    if(bulb._sysInfo.is_color === 1) {
-      bulb_count++;
+    if (bulb._sysInfo.is_color === 1) {
+      bulbCount++
       res.write('<h3>' + bulb.alias +
-        ' <a href="sunrise.html?bulb=' + encodeURIComponent(bulb.alias).replace(/'/g, "%27") +
+        ' <a href="sunrise.html?bulb=' + encodeURIComponent(bulb.alias).replace(/'/g, '%27') +
         '">Sunrise Settings</a> <button onclick="ajax_get(\'off?' + bulb.host +
-        '\')">Off</button> <button onclick="ajax_get(\'demo?' + encodeURIComponent(bulb.alias).replace(/'/g, "%27") + 
-        '\')">Demo</button></h3>\n');
+        '\')">Off</button> <button onclick="ajax_get(\'demo?' + encodeURIComponent(bulb.alias).replace(/'/g, '%27') +
+        '\')">Demo</button></h3>\n')
     }
-  });
-  await sleeper;
-  client.stopDiscovery();
-  if(bulb_count == 0) res.write("<h3>No color bulbs found!</h3>\n");
-  res.write('<div style="text-align: left; margin-top: 2em;"><a href="https://github.com/alwynallan/sunrise">Source on GitHub</a></div>\n');
-  res.write('</div></div>\n</body>\n</html>\n');
-  res.end();
+  })
+  await sleeper
+  client.stopDiscovery()
+  if (bulbCount === 0) res.write('<h3>No color bulbs found!</h3>\n')
+  res.write('<div style="text-align: left; margin-top: 2em;"><a href="https://github.com/alwynallan/sunrise">Source on GitHub</a></div>\n')
+  res.write('</div></div>\n</body>\n</html>\n')
+  res.end()
 }
 
-function index_page(res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<!DOCTYPE html>\n');
-  res.write('<html><head><title>Index</title>\n');
-  res.write('<link rel="stylesheet" href="demo.css">\n');
-  res.write('<link rel="stylesheet" href="form-basic.css">\n');
-  res.write('</head>\n<body>\n');
-  res.write('<header><h1>Index of node subsystem</h1></header>\n');
-  res.write('<div class="main-content"><div class="form-basic">\n');
-  res.write('<h3><a href="bulbs.html">bulbs.html</a></h3><br>\n');
-  res.write('<p>whoami: ' + cp.execSync('whoami') + '</p>\n');
-  res.write('</div></div>\n');
-  res.write('</body></html>');
-  res.end();
+function indexPage (res) {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.write('<!DOCTYPE html>\n')
+  res.write('<html><head><title>Index</title>\n')
+  res.write('<link rel="stylesheet" href="demo.css">\n')
+  res.write('<link rel="stylesheet" href="form-basic.css">\n')
+  res.write('</head>\n<body>\n')
+  res.write('<header><h1>Index of node subsystem</h1></header>\n')
+  res.write('<div class="main-content"><div class="form-basic">\n')
+  res.write('<h3><a href="bulbs.html">bulbs.html</a></h3><br>\n')
+  res.write('<p>whoami: ' + cp.execSync('whoami') + '</p>\n')
+  res.write('</div></div>\n')
+  res.write('</body></html>')
+  res.end()
 }
 
-function css_file(filename, res) {
-  fs.readFile(filename, function(error, content) {
+function cssFile (filename, res) {
+  fs.readFile(filename, function (error, content) {
     if (error) {
-      res.writeHead(500);
-      res.end();
+      res.writeHead(500)
+      res.end()
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/css'})
+      res.end(content, 'utf-8')
     }
-    else {                   
-      res.writeHead(200, {'Content-Type': 'text/css'});
-      res.end(content, 'utf-8');                  
-    }
-  });
+  })
 }
 
-function html_file(filename, res) {
-  fs.readFile(filename, function(error, content) {
+function htmlFile (filename, res) {
+  fs.readFile(filename, function (error, content) {
     if (error) {
-      res.writeHead(500);
-      res.end();
+      res.writeHead(500)
+      res.end()
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.end(content, 'utf-8')
     }
-    else {                   
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(content, 'utf-8');                  
-    }
-  });
+  })
 }
 
-function do_off(host_ip) {
-  var client = new Client();
-  client.getDevice({host: host_ip}).then((device) => {
+function doOff (hostIP) {
+  var client = new Client()
+  client.getDevice({host: hostIP}).then((device) => {
     device.lighting.setLightState({
       transition_period: 500,
       on_off: false
-    });
-  });
+    })
+  })
 }
 
-function do_demo(bulbName) {
-  let key = 'bulb_' + bulbName + '.json';
-  let value = storage.getItemSync(key);
-  cp.fork('sunrise-tp.js', [bulbName, '2', value.color1, value.color2, value.final]);
+function doDemo (bulbName) {
+  let key = 'bulb_' + bulbName + '.json'
+  let value = storage.getItemSync(key)
+  cp.fork('sunrise-tp.js', [bulbName, '2', value.color1, value.color2, value.final])
 }
 
-function do_backend(req, res) {
+function doBackend (req, res) {
   // replaces sunrise_backend.php, assumes POST
-  var jsonString = '';
+  var jsonString = ''
   req.on('data', function (data) {
-     jsonString += data;
-  });
+    jsonString += data
+  })
   req.on('end', function () {
-    let obj = JSON.parse(jsonString);
-    obj.bulb = decodeURIComponent(obj.bulb);
-    let key = 'bulb_' + obj.bulb + '.json';
-    switch(obj.command) {
+    let obj = JSON.parse(jsonString)
+    obj.bulb = decodeURIComponent(obj.bulb)
+    let key = 'bulb_' + obj.bulb + '.json'
+    switch (obj.command) {
       case 'save':
-        delete obj.command;
-        delete obj.default;
-        storage.setItemSync(key, obj);
-        //console.log("The config was saved!");
-        if(tasks.hasOwnProperty(obj.bulb)){
-          tasks[obj.bulb].destroy();
-          delete tasks[obj.bulb];
+        delete obj.command
+        delete obj.default
+        storage.setItemSync(key, obj)
+        // console.log("The config was saved!");
+        if (tasks.hasOwnProperty(obj.bulb)) {
+          tasks[obj.bulb].destroy()
+          delete tasks[obj.bulb]
         }
-        addCron(obj); // checks if it is active and adds it back if it is
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('File saved: ' + key);
-        break;
+        addCron(obj) // checks if it is active and adds it back if it is
+        res.writeHead(200, {'Content-Type': 'text/plain'})
+        res.end('File saved: ' + key)
+        break
       case 'load':
-        if(storage.keys().indexOf(key) === -1) {
-          let def = {default: true, active: true, start: '05:30', duration: '30',
-                     mon: true, tue: true,  wed:true, thu: true, fri: true, sat: false, sun: false,
-                     color1: '#be45be', color2: '#f3ae52', final: '100'};
-          def.bulb = obj.bulb;
-          let deftxt = JSON.stringify(def);
-          storage.setItemSync(key, def);
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end(deftxt);
+        if (storage.keys().indexOf(key) === -1) {
+          let def = {default: true,
+            active: true,
+            start: '05:30',
+            duration: '30',
+            mon: true,
+            tue: true,
+            wed: true,
+            thu: true,
+            fri: true,
+            sat: false,
+            sun: false,
+            color1: '#be45be',
+            color2: '#f3ae52',
+            final: '100'}
+          def.bulb = obj.bulb
+          let deftxt = JSON.stringify(def)
+          storage.setItemSync(key, def)
+          res.writeHead(200, {'Content-Type': 'text/plain'})
+          res.end(deftxt)
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/plain'})
+          res.end(JSON.stringify(storage.getItemSync(key)))
         }
-        else {
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end(JSON.stringify(storage.getItemSync(key)));
-        }
-        break;
+        break
       default:
-        console.log('error: invalid command');
-        console.log(obj);
-        break;
+        console.log('error: invalid command')
+        console.log(obj)
+        break
     }
-  });
+  })
 }
 
-function responer(req, res) {
-  let [page, args] = req.url.split('?',2);
-  //console.log('page: ' + page + ' args: ' + args);
+function responer (req, res) {
+  let [page, args] = req.url.split('?', 2)
+  // console.log('page: ' + page + ' args: ' + args);
   switch (page) {
     case '/bulbs.html':
-      bulb_page(res);
-      break;
+      bulbPage(res)
+      break
     case '/':
     case '/index.html':
-      index_page(res);
-      break;
+      indexPage(res)
+      break
     case '/sunrise.html':
-      html_file(page.substr(1), res);
-      break;
+      htmlFile(page.substr(1), res)
+      break
     case '/demo.css':
     case '/form-basic.css':
-      css_file(page.substr(1), res);
-      break;
+      cssFile(page.substr(1), res)
+      break
     case '/off':
-      do_off(args);
-      res.end();
-      break;
+      doOff(args)
+      res.end()
+      break
     case '/demo':
-      do_demo(decodeURIComponent(args));
-      res.end();
-      break;
+      doDemo(decodeURIComponent(args))
+      res.end()
+      break
     case '/backend':
-      do_backend(req, res);
-      break;
+      doBackend(req, res)
+      break
     case '/wipe.i.know.what.im.doing':
-      storage.clearSync();
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Your storage is wiped out!');
-      break;
+      storage.clearSync()
+      res.writeHead(200, {'Content-Type': 'text/plain'})
+      res.end('Your storage is wiped out!')
+      break
     default:
-      res.writeHead(404);
-      res.end();
-      break;
+      res.writeHead(404)
+      res.end()
+      break
   }
 }
 
 function sunriseSim (bulbName) {
-  //console.log('Doing sim for ' + bulbName);
-  let key = 'bulb_' + bulbName + '.json';
-  let value = storage.getItemSync(key);
-  cp.fork('sunrise-tp.js', [bulbName, value.duration, value.color1, value.color2, value.final]);
+  // console.log('Doing sim for ' + bulbName);
+  let key = 'bulb_' + bulbName + '.json'
+  let value = storage.getItemSync(key)
+  cp.fork('sunrise-tp.js', [bulbName, value.duration, value.color1, value.color2, value.final])
 }
 
-function addCron(v) {
-  //console.log(v);
-  if(v.active){
-    let parts = v.start.split(":",2);
-    if (isNaN(parts[0]) || parts[0] === '') parts[0] = 0; // cron is very picky
-    if (isNaN(parts[1]) || parts[1] === '') parts[1] = 0;
-    let hour = parts[0];
-    let min = parts[1];
-    let wday = (v.sun?'0,':'') + (v.mon?'1,':'') + (v.tue?'2,':'') + (v.wed?'3,':'') + (v.thu?'4,':'') + (v.fri?'5,':'') + (v.sat?'6,':'');
-    wday = wday.replace(/,$/, '');
-    if(wday === '') wday = '*'; // none is all
-    let cronStr = '0 ' + parseInt(min) + ' ' + parseInt(hour) + ' * * ' + wday;
-    //console.log('scheduling ' + v.bulb + ' at ' + cronStr);
-    tasks[v.bulb] = cron.schedule(cronStr, function(){sunriseSim(v.bulb);});
+function addCron (v) {
+  // console.log(v);
+  if (v.active) {
+    let parts = v.start.split(':', 2)
+    if (isNaN(parts[0]) || parts[0] === '') parts[0] = 0 // cron is very picky
+    if (isNaN(parts[1]) || parts[1] === '') parts[1] = 0
+    let hour = parts[0]
+    let min = parts[1]
+    let wday = (v.sun ? '0,' : '') + (v.mon ? '1,' : '') + (v.tue ? '2,' : '') + (v.wed ? '3,' : '') + (v.thu ? '4,' : '') + (v.fri ? '5,' : '') + (v.sat ? '6,' : '')
+    wday = wday.replace(/,$/, '')
+    if (wday === '') wday = '*' // none is all
+    let cronStr = '0 ' + parseInt(min) + ' ' + parseInt(hour) + ' * * ' + wday
+    // console.log('scheduling ' + v.bulb + ' at ' + cronStr);
+    tasks[v.bulb] = cron.schedule(cronStr, function () { sunriseSim(v.bulb) })
   }
 }
 
-var tasks = {};
-storage.initSync();
-for (var i=0; i < storage.values().length; i++) addCron(storage.values()[i]);
-var server = http.createServer(responer);
-server.listen(8000);
+var tasks = {}
+storage.initSync()
+for (var i = 0; i < storage.values().length; i++) addCron(storage.values()[i])
+var server = http.createServer(responer)
+server.listen(8000)
--- sunrise-tp.js
+++ sunrise-tp.js
@@ -1,203 +1,206 @@
 // Copyright (C) 2017 alwynallan@gmail.com
 // Merry Christmas Sue!
 
+// This is forked from master.js but can also be invoked from the command line for testing
+// e.g. 'node sunrise-tp "Peter's Lamp"'
+
 const {
   Client
-} = require('tplink-smarthome-api');
+} = require('tplink-smarthome-api')
 
-const client = new Client();
+const client = new Client()
 
-const steps12 = 50;
-const steps23 = 50;
-const steps_ct = 60;
-const total_steps = steps12 + steps23 + steps_ct;
+const steps12 = 50
+const steps23 = 50
+const stepsCtt = 60
+const totalSteps = steps12 + steps23 + stepsCtt
 
-//var host=null;
-const bulb_value = decodeURIComponent(process.argv[2]); // should warn if it's not there
-const duration_value = parseInt(process.argv[3]) || 45;
-const color1_rgbh = process.argv[4] || "#07078f";
-const color2_rgbh = process.argv[5] || "#f3ae52";
-const final_bright = parseInt(process.argv[6]) || 100;
-const color1_hsv = rgb2hsv(hexToR(color1_rgbh), hexToG(color1_rgbh),
-  hexToB(color1_rgbh));
-const color2_hsv = rgb2hsv(hexToR(color2_rgbh), hexToG(color2_rgbh),
-  hexToB(color2_rgbh));
-const color3_hsv = [60, 18, 100]; // visually similar to 2700
-const ct_start = 2700;
-const ct_bump = 15;
+// var host=null;
+const bulbValue = decodeURIComponent(process.argv[2]) // should warn if it's not there
+const durationValue = parseInt(process.argv[3]) || 45
+const color1RGBH = process.argv[4] || '#07078f'
+const color2RGBH = process.argv[5] || '#f3ae52'
+const finalBright = parseInt(process.argv[6]) || 100
+const color1HSV = rgb2hsv(hexToR(color1RGBH), hexToG(color1RGBH),
+  hexToB(color1RGBH))
+const color2HSV = rgb2hsv(hexToR(color2RGBH), hexToG(color2RGBH),
+  hexToB(color2RGBH))
+const color3HSV = [60, 18, 100] // visually similar to 2700
+const ctStart = 2700
+const ctBump = 15
 
-//function sleep(ms) { // useful for development
+// function sleep(ms) { // useful for development
 //  return new Promise(resolve => setTimeout(resolve, ms));
-//}
+// }
 
 // http://www.javascripter.net/faq/hextorgb.htm
-function hexToR(h) {
+function hexToR (h) {
   return parseInt((cutHex(h)).substring(0, 2), 16)
 }
 
-function hexToG(h) {
+function hexToG (h) {
   return parseInt((cutHex(h)).substring(2, 4), 16)
 }
 
-function hexToB(h) {
+function hexToB (h) {
   return parseInt((cutHex(h)).substring(4, 6), 16)
 }
 
-function cutHex(h) {
-  return (h.charAt(0) == "#") ? h.substring(1, 7) : h
+function cutHex (h) {
+  return (h.charAt(0) === '#') ? h.substring(1, 7) : h
 }
 
 // http://www.javascripter.net/faq/rgb2hsv.htm
-function rgb2hsv(r, g, b) {
-  var computedH = 0;
-  var computedS = 0;
-  var computedV = 0;
-  r /= 255.0;
-  g /= 255.0;
-  b /= 255.0;
-  var minRGB = Math.min(r, Math.min(g, b));
-  var maxRGB = Math.max(r, Math.max(g, b));
+function rgb2hsv (r, g, b) {
+  var computedH = 0
+  var computedS = 0
+  var computedV = 0
+  r /= 255.0
+  g /= 255.0
+  b /= 255.0
+  var minRGB = Math.min(r, Math.min(g, b))
+  var maxRGB = Math.max(r, Math.max(g, b))
 
   // Black-gray-white
-  if (minRGB == maxRGB) {
-    computedV = minRGB;
-    return [0, 0, parseInt(computedV * 100.99)];
+  if (minRGB === maxRGB) {
+    computedV = minRGB
+    return [0, 0, parseInt(computedV * 100.99)]
   }
 
   // Colors other than black-gray-white:
-  var d = (r == minRGB) ? g - b : ((b == minRGB) ? r - g : b - r);
-  var h = (r == minRGB) ? 3 : ((b == minRGB) ? 1 : 5);
-  computedH = 60 * (h - d / (maxRGB - minRGB));
-  computedS = (maxRGB - minRGB) / maxRGB;
-  computedV = maxRGB;
+  var d = (r === minRGB) ? g - b : ((b === minRGB) ? r - g : b - r)
+  var h = (r === minRGB) ? 3 : ((b === minRGB) ? 1 : 5)
+  computedH = 60 * (h - d / (maxRGB - minRGB))
+  computedS = (maxRGB - minRGB) / maxRGB
+  computedV = maxRGB
   return [parseInt(computedH), parseInt(computedS * 100.99), parseInt(
-    computedV * 100.99)];
+    computedV * 100.99)]
 }
 
-function cblend(ratio, color0, color1) {
-  if (ratio < 0.0) ratio = 0.0;
-  if (ratio > 1.0) ratio = 1.0;
+function cblend (ratio, color0, color1) {
+  if (ratio < 0.0) ratio = 0.0
+  if (ratio > 1.0) ratio = 1.0
   if (Math.abs(color0[0] - color1[0]) > 180.0) { // transition through hue=360
-    let mov = color0[0] < color1[0] ?
-        -((color0[0] - 0.0) + (360.0 - color1[0])) : 
-        (color1[0] - 0.0) + (360.0 - color0[0]);
-    let hue = color0[0] + ratio * mov;
-    if (hue > 360.0) hue -= 360.0;
-    if (hue < 0.0) hue += 360.0;
+    let mov = color0[0] < color1[0]
+        ? -((color0[0] - 0.0) + (360.0 - color1[0]))
+        : (color1[0] - 0.0) + (360.0 - color0[0])
+    let hue = color0[0] + ratio * mov
+    if (hue > 360.0) hue -= 360.0
+    if (hue < 0.0) hue += 360.0
     return [parseInt(hue),
-            parseInt((1.0 - ratio) * color0[1] + ratio * color1[1]),
-            parseInt((1.0 - ratio) * color0[2] + ratio * color1[2])
-    ];
+      parseInt((1.0 - ratio) * color0[1] + ratio * color1[1]),
+      parseInt((1.0 - ratio) * color0[2] + ratio * color1[2])
+    ]
   }
   return [parseInt((1.0 - ratio) * color0[0] + ratio * color1[0]),
-          parseInt((1.0 - ratio) * color0[1] + ratio * color1[1]),
-          parseInt((1.0 - ratio) * color0[2] + ratio * color1[2])
-  ];
+    parseInt((1.0 - ratio) * color0[1] + ratio * color1[1]),
+    parseInt((1.0 - ratio) * color0[2] + ratio * color1[2])
+  ]
 }
 
-var bump_level_b;
-var bump_level_device;
-var bump_level_timer = null;
-var bump_level_transition;
-var bump_level_been_seen_on = false;
+var bumpLevelB
+var bumpDevelDevice
+var bumpLevelTimer = null
+var bumpLevelTransition
+var bumpLevelBeenSeenON = false
 
-async function bump_level() {
-  if (bump_level_b > 0) {
-    let state = await bump_level_device.getPowerState();
-    if (!state && bump_level_been_seen_on) {
-      //console.log("User has turned the bulb off, exiting.");
-      process.exit();
+async function bumpLevel () {
+  if (bumpLevelB > 0) {
+    let state = await bumpDevelDevice.getPowerState()
+    if (!state && bumpLevelBeenSeenON) {
+      // console.log("User has turned the bulb off, exiting.");
+      process.exit()
     }
-    if(state) bump_level_been_seen_on = true;
+    if (state) bumpLevelBeenSeenON = true
   }
-  if (bump_level_b <= steps12) {
-    let color = cblend(bump_level_b / 50.0, color1_hsv, color2_hsv);
-    let h = color[0];
-    let s = color[1];
-    let v = parseInt((bump_level_b * (final_bright - 1)) / 100 + 1);
-    //console.log('h:' + h + ' s:' + s + ' v:' + v);
-    await bump_level_device.lighting.setLightState({
+  if (bumpLevelB <= steps12) {
+    let color = cblend(bumpLevelB / 50.0, color1HSV, color2HSV)
+    let h = color[0]
+    let s = color[1]
+    let v = parseInt((bumpLevelB * (finalBright - 1)) / 100 + 1)
+    // console.log('h:' + h + ' s:' + s + ' v:' + v);
+    await bumpDevelDevice.lighting.setLightState({
       hue: h,
       saturation: s,
       brightness: v,
       color_temp: 0,
-      transition_period: bump_level_transition,
+      transition_period: bumpLevelTransition,
       on_off: true
-    });
-    bump_level_b++;
-  } else if (bump_level_b <= (steps12 + steps23)) {
-    let color = cblend((bump_level_b - 50.0) / 50.0, color2_hsv,
-      color3_hsv);
-    let h = color[0];
-    let s = color[1];
-    let v = parseInt((bump_level_b * (final_bright - 1)) / 100 + 1);
-    //console.log('h:' + h + ' s:' + s + ' v:' + v;
-    await bump_level_device.lighting.setLightState({
+    })
+    bumpLevelB++
+  } else if (bumpLevelB <= (steps12 + steps23)) {
+    let color = cblend((bumpLevelB - 50.0) / 50.0, color2HSV,
+      color3HSV)
+    let h = color[0]
+    let s = color[1]
+    let v = parseInt((bumpLevelB * (finalBright - 1)) / 100 + 1)
+    // console.log('h:' + h + ' s:' + s + ' v:' + v;
+    await bumpDevelDevice.lighting.setLightState({
       hue: h,
       saturation: s,
       brightness: v,
       color_temp: 0,
-      transition_period: bump_level_transition,
+      transition_period: bumpLevelTransition,
       on_off: true
-    });
-    bump_level_b++;
-  } else if (bump_level_b <= total_steps) {
-    let ct = ct_start + (bump_level_b - 101) * ct_bump;
-    //console.log('ct:' + ct);
-    await bump_level_device.lighting.setLightState({
+    })
+    bumpLevelB++
+  } else if (bumpLevelB <= totalSteps) {
+    let ct = ctStart + (bumpLevelB - 101) * ctBump
+    // console.log('ct:' + ct);
+    await bumpDevelDevice.lighting.setLightState({
       hue: 0,
       saturation: 0,
-      brightness: final_bright,
+      brightness: finalBright,
       color_temp: ct,
-      transition_period: bump_level_transition,
+      transition_period: bumpLevelTransition,
       on_off: true
-    });
-    bump_level_b++;
+    })
+    bumpLevelB++
   } else {
-    clearInterval(bump_level_timer);
-    //console.log("All done!");
+    clearInterval(bumpLevelTimer)
+    // console.log("All done!");
   }
 }
 
-async function do_cycle(device) {
-  bump_level_device = device;
-  bump_level_b = 0;
-  let pause_ms = (duration_value * 60000) / total_steps;
-  bump_level_transition = pause_ms-100;
-  setImmediate(bump_level);
-  bump_level_timer = setInterval(bump_level, pause_ms);
+async function doCycle (device) {
+  bumpDevelDevice = device
+  bumpLevelB = 0
+  let pauseMs = (durationValue * 60000) / totalSteps
+  bumpLevelTransition = pauseMs - 100
+  setImmediate(bumpLevel)
+  bumpLevelTimer = setInterval(bumpLevel, pauseMs)
 }
 
 // catch devices, look for the one we want
 client.on('device-new', (device) => {
-  device.getSysInfo().then(function(info) {
-    if (info.alias === bulb_value) {
-      //console.log(device);
-      //console.log(info);
-      endDiscovery();
-      do_cycle(device);
+  device.getSysInfo().then(function (info) {
+    if (info.alias === bulbValue) {
+      // console.log(device);
+      // console.log(info);
+      endDiscovery()
+      doCycle(device)
     }
-  });
-});
+  })
+})
 
-var discovery_timer = null;
-var retries = 0;
+var discoveryTimer = null
+var retries = 0
 
 var endDiscovery = () => {
-  clearTimeout(discovery_timer);
-  client.stopDiscovery();
+  clearTimeout(discoveryTimer)
+  client.stopDiscovery()
 }
 
 var retryDiscovery = () => {
-  if(retries) client.stopDiscovery();
-  retries++;
-  if(retries < 6) {
-    //console.log("Beginning try " + retries);
-    client.startDiscovery();
-    discovery_timer = setTimeout(retryDiscovery, 400);
+  if (retries) client.stopDiscovery()
+  retries++
+  if (retries < 6) {
+    // console.log("Beginning try " + retries);
+    client.startDiscovery()
+    discoveryTimer = setTimeout(retryDiscovery, 400)
   }
 }
 
 // It would be nice, and possibly more reliable, to store and use each bulb's IP and use it with client.getDevice({host: host_ip}), but
-// the IP could change over time, particularly if it assigned to a different WiFi network. So, 
-retryDiscovery();
+// the IP could change over time, particularly if it assigned to a different WiFi network. So,
+retryDiscovery()
